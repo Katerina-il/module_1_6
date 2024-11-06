7253e58eaa82364b0ae140bb0dc2acd302510ee5
Katerina-il <kkaterina.iln@gmail.com>
2024-11-06 21:46:38 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+# пример 1
+number_of_homework= 12
+number_of_hours=1.5
+CourseName="Python"
+average_execution_time =(number_of_hours/float(number_of_homework))
+print("Курс:",CourseName,", всего задач: ", number_of_hours, ", затрачено часов:", number_of_hours, ", среднее время выполнения:", average_execution_time,".")
